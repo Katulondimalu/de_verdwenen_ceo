be9197ae3aa5ceadb5131a3d46c84212e89388c8
Katulondimalu <104638030+Katulondimalu@users.noreply.github.com>
2022-11-22 15:45:00 +0100

    gaan we doen

--- src/pages/SplashScreen.js
+++ src/pages/SplashScreen.js
@@ -27,7 +27,7 @@ const SplashScreen = () => {
           marginRight: 16,
         }}
       >
-        <h2>{t('De Verdwenen CEO')}</h2>
+        <h2>{t('The Lost CEO')}</h2>
       </div>
       <img
         // @ts-ignore
